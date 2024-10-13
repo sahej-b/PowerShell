0eac5b1d6bcb0fa39c000dad3d6b0e215b47872e
Sahej <87836835+sahej-b@users.noreply.github.com>
2024-10-14 09:03:59 +1000

    Update list-recycle-bin.ps1

--- scripts/list-recycle-bin.ps1
+++ scripts/list-recycle-bin.ps1
@@ -1,7 +1,7 @@
 try {
-	(New-Object -ComObject Shell.Application).NameSpace(0x0a).Items() | Select-Object Name,Size,Path
-	exit 0 # success
+    (New-Object -ComObject Shell.Application).NameSpace(0x0a).Items() | Select-Object Name,Size,Path
+    Read-Host -Prompt "Press Enter to close"
 } catch {
-	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
-	exit 1
+    "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+    Read-Host -Prompt "An error occurred. Press Enter to close"
 }
