fd8718ff165e37342a64a0ab258c7c615177f6f7
Sahej <87836835+sahej-b@users.noreply.github.com>
2024-10-14 09:07:18 +1000

    Update get-sha256.ps1

--- scripts/get-sha256.ps1
+++ scripts/get-sha256.ps1
@@ -1,13 +1,15 @@
 param([string]$file = "")
 
 try {
-	if ($file -eq "" ) { $file = Read-Host "Enter the filename" }
+	if ($file -eq "" ) { 
+        $file = Read-Host "Enter the filename" 
+    }
 
 	$Result = get-filehash $file -algorithm SHA256
 
 	"✅ SHA256 hash is: $($Result.Hash)"
-	exit 0 # success
+	Read-Host -Prompt "Press Enter to close"
 } catch {
 	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
-	exit 1
+	Read-Host -Prompt "An error occurred. Press Enter to close"
 }
