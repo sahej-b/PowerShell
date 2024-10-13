4d35f756774b1990594b78b6cc3436e55129767c
Sahej <87836835+sahej-b@users.noreply.github.com>
2024-10-14 08:55:25 +1000

    Update list-crypto-rates.ps1

--- scripts/list-crypto-rates.ps1
+++ scripts/list-crypto-rates.ps1
@@ -1,21 +1,3 @@
-﻿<#
-.SYNOPSIS
-	List crypto rates
-.DESCRIPTION
-	This PowerShell script queries cryptocompare.com and lists the current crypto exchange rates in USD/EUR/RUB/CNY.
-.EXAMPLE
-	PS> ./list-crypto-rates.ps1
-
-	Cryptocurrency               USD                    EUR                    RUB                    CNY
-	--------------               ---                    ---                    ---                    ---
-	1 Bitcoin (BTC) =            29054.01               26552.23               2786627.84             172521.27
-	...
-.LINK
-	https://github.com/fleschutz/PowerShell
-.NOTES
-	Author: Markus Fleschutz | License: CC0
-#>
-
 function ListCryptoRate { param([string]$Symbol, [string]$Name)
 	$Rates = (Invoke-WebRequest -URI "https://min-api.cryptocompare.com/data/price?fsym=$Symbol&tsyms=USD,EUR,RUB,CNY" -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
 	New-Object PSObject -property @{ 'Cryptocurrency' = "1 $Name ($Symbol) ="; 'USD' = "$($Rates.USD)"; 'EUR' = "$($Rates.EUR)"; 'RUB' = "$($Rates.RUB)"; 'CNY' = "$($Rates.CNY)" }
