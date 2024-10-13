e0eef5422624c7e04c36f90b22bf69dd716d728b
Sahej <87836835+sahej-b@users.noreply.github.com>
2024-10-14 09:02:53 +1000

    Update list-recycle-bin.ps1

--- scripts/list-recycle-bin.ps1
+++ scripts/list-recycle-bin.ps1
@@ -1,16 +1,3 @@
-﻿<#
-.SYNOPSIS
-	Lists the content of the recycle bin folder
-.DESCRIPTION
-	This PowerShell script lists the content of the recycle bin folder.
-.EXAMPLE
-	PS> ./list-recycle-bin.ps1
-.LINK
-	https://github.com/fleschutz/PowerShell
-.NOTES
-	Author: Markus Fleschutz | License: CC0
-#>
-
 try {
 	(New-Object -ComObject Shell.Application).NameSpace(0x0a).Items() | Select-Object Name,Size,Path
 	exit 0 # success
