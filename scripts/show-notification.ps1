893c270b53c77e174f2bd7030c1fed3837ad2b35
Sahej <87836835+sahej-b@users.noreply.github.com>
2024-10-14 12:35:04 +1000

    Update show-notification.ps1

--- scripts/show-notification.ps1
+++ scripts/show-notification.ps1
@@ -1,4 +1,4 @@
-param([string]$text = "go to the party goober", [string]$title = "", [int]$Duration = 5000)
+param([string]$text = "go to the party goober", [string]$title = "beetham", [int]$Duration = 5000)
 
 try {
 	Add-Type -AssemblyName System.Windows.Forms 
